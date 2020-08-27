6a37d2a47f48b2b59508ed0708e38155ddd79a0a
vinoca <vinoca@vinoca.org>
2020-08-27 11:39:27 +0800

    refactor Forward, remove route with domain, just domain substitute

--- README.md
+++ README.md
@@ -9,20 +9,31 @@ listen_address: 127.0.0.1:3003
 # optional, if set, will forward all connect to this proxy
 socks5_server: 127.0.0.1:1080
 domain_name:
-  # default scheme is https
   x.com: www.google.com
-  y.com: http://wikipedia.org:8080
+  y.com: wikipedia.org
 ```
 
 with nginx:
 
 ```nginx
     server {
-        server_name x.com;
+        server_name x.com *.x.com y.com *.y.com;
 
         location / { 
             proxy_http_version 1.1;
             proxy_set_header Host $http_host;
+            proxy_pass http://127.0.0.1:3003;
+        }
+    }
+    server {
+        listen 443 ssl;
+        server_name x.com *.x.com y.com *.y.com;
+		# add tls certificate here
+
+        location / { 
+            proxy_http_version 1.1;
+            proxy_set_header host $http_host;
+            proxy_set_header X-Scheme https;
             proxy_pass http://127.0.0.1:3003;
         }
     }
--- src/server.rs
+++ src/server.rs
@@ -1,206 +1,61 @@
 use std::{
     collections::HashMap,
-    convert::{TryFrom, TryInto},
     net::{SocketAddr, TcpListener, TcpStream, ToSocketAddrs},
 };
 
-use anyhow::{anyhow, Error, Result};
-use async_compression::futures::bufread::{
-    BrotliDecoder, BrotliEncoder, DeflateDecoder, DeflateEncoder, GzipDecoder, GzipEncoder,
-};
-use http_types::{
-    headers::HeaderValue, Body, Error as HttpError, Request, Response, StatusCode, Url,
-};
+use anyhow::Result;
+use http_types::{Body, Error as HttpError, Request, Response, StatusCode};
 use smol::{io::AsyncRead, Async, Task};
 
 use crate::constants::{CONFIG, FORWARD};
 
-struct Target {
-    scheme: String,
-    host: String,
-    port: u16,
-}
-
-impl Target {
-    fn scheme(&self) -> &str {
-        &self.scheme
-    }
-
-    fn host(&self) -> &str {
-        &self.host
-    }
-
-    fn port(&self) -> u16 {
-        self.port
-    }
-
-    async fn address(&self) -> Result<SocketAddr> {
-        let host = self.host.to_string();
-        let port = self.port;
-        smol::unblock!((host.as_str(), port)
-            .to_socket_addrs()?
-            .next()
-            .ok_or(anyhow!("invalid domain")))
-    }
-
-    fn fuse_request(&self, req: Request) -> Result<Request> {
-        let mut req = req;
-        req.insert_header("host", self.host());
-        let dest_url = req.url_mut();
-        dest_url
-            .set_scheme(self.scheme())
-            .map_err(|_| anyhow!("set scheme error"))?;
-        dest_url.set_host(Some(self.host()))?;
-        dest_url
-            .set_port(Some(self.port))
-            .map_err(|_| anyhow!("set port error"))?;
-        Ok(req)
-    }
-
-    fn host_with_port(&self) -> String {
-        if (self.scheme == "http" && self.port == 80)
-            || (self.scheme == "https" && self.port == 443)
-        {
-            self.host.to_string()
-        } else {
-            format!("{}:{}", self.host, self.port)
-        }
-    }
-}
-
-impl TryFrom<&str> for Target {
-    type Error = Error;
-
-    fn try_from(s: &str) -> Result<Target> {
-        let s = if s.find("://").is_some() {
-            s.to_string()
-        } else {
-            format!("https://{}", s)
-        };
-        let url: Url = s.parse()?;
-        let host = url.host_str().ok_or(anyhow!("invalid domain"))?;
-        let port = url
-            .port_or_known_default()
-            .ok_or(anyhow!("invalid domain"))?;
-        Ok(Target {
-            scheme: url.scheme().to_string(),
-            host: host.to_string(),
-            port,
-        })
-    }
-}
-
+#[derive(Debug)]
 pub struct Forward<'a> {
-    domain: HashMap<&'a str, Target>,
+    domain: &'a HashMap<String, String>,
 }
 
 impl<'a> Forward<'a> {
-    pub fn new(domain_name: &'a HashMap<String, String>) -> Result<Forward<'a>> {
-        let mut domain = HashMap::new();
-        for (k, v) in domain_name {
-            let target = v.as_str().try_into()?;
-            domain.insert(k.as_str(), target);
-        }
+    pub fn new(domain: &'a HashMap<String, String>) -> Result<Forward<'a>> {
         Ok(Forward { domain })
     }
 
-    pub async fn forward(&self, req: Request) -> http_types::Result<Response> {
-        let url = req.url();
-        let domain = match url.domain() {
-            Some(h) => h,
-            None => return Err(http_error("missing domain".to_string())),
-        };
-        match self.domain.get(domain) {
-            Some(domain) => self.request(req, domain).await,
-            None => return Err(http_error("invalid domain, check config file".to_string())),
+    async fn forward(&self, req: Request) -> http_types::Result<Response> {
+        let mut req = req;
+        let query: Vec<_> = req
+            .url()
+            .query_pairs()
+            .map(|(q, v)| {
+                let s = self.restore_domain(&v);
+                format!("{}={}", q, s)
+            })
+            .collect();
+        let query = query.join("&");
+        let scheme = req.header("X-Scheme").map(|i| i.as_str().to_string());
+        let path = req.url().path();
+        let path = self.restore_domain(path);
+        let url = req.url_mut();
+        if !query.is_empty() {
+            url.set_query(Some(&query));
         }
-    }
-
-    async fn request(&self, req: Request, target: &Target) -> http_types::Result<Response> {
-        let host = target.host();
-        let addr = target
-            .address()
-            .await
-            .map_err(|_| http_error("invalid target".to_string()))?;
-        let req = target
-            .fuse_request(req)
-            .map_err(|e| http_error(e.to_string()))?;
-
-        let stream = match &CONFIG.socks5_server {
-            Some(server) => {
-                let server = server.clone();
-                let server = smol::unblock!(server
-                    .to_socket_addrs()?
-                    .next()
-                    .ok_or(anyhow!("invalid host")))?;
-                socks5::connect_without_auth(server, (host.to_string(), target.port()).into())
-                    .await?
-            }
-            None => Async::<TcpStream>::connect(addr).await?,
-        };
-
-        let mut resp = match target.scheme() {
-            "https" => {
-                let stream = async_native_tls::connect(host, stream).await?;
-                async_h1::connect(stream, req).await?
-            }
-            "http" => async_h1::connect(stream, req).await?,
-            s => return Err(http_error(format!("unsupported scheme: {}", s))),
-        };
-
-        if let Some(location) = resp.header("location") {
-            let mut location = location.as_str().to_string();
-            for (k, v) in &self.domain {
-                location = location.replace(&v.host_with_port(), k);
-            }
-            resp.insert_header("location", location);
+        if let Some(scheme) = scheme {
+            url.set_scheme(&scheme)
+                .map_err(|_| http_error("invalid request"))?;
         }
-
-        if let Some(referer) = resp.header("referer") {
-            let mut referer = referer.as_str().to_string();
-            for (k, v) in &self.domain {
-                referer = referer.replace(&v.host_with_port(), k);
-            }
-            resp.insert_header("referer", referer);
+        url.set_path(&path);
+        if let Some(host) = url.host_str() {
+            let host = self.restore_domain(host);
+            url.set_host(Some(&host))?;
+            req.insert_header("host", host);
         }
+        self.restore_header(&mut req);
 
-        if let Some(cookie) = resp.header("set-cookie") {
-            let cookie: Vec<_> = cookie
-                .iter()
-                .map(|i| {
-                    let i = i.as_str();
-                    let i: Vec<_> = i
-                        .split(';')
-                        .filter(|i| {
-                            let i = i.trim_start();
-                            !(i.len() > 7 && i[..7].to_lowercase() == "domain=")
-                        })
-                        .collect();
-                    let i = i.join(";");
-                    unsafe { HeaderValue::from_bytes_unchecked(i.as_bytes().to_vec()) }
-                })
-                .collect();
-            resp.insert_header("set-cookie", cookie.as_slice());
-        }
-
-        if resp.status() == StatusCode::NotModified {
-            return Ok(resp);
-        }
-
-        Coder::De.code(&mut resp);
-
-        // replace domain
-        if let Some(content_type) = resp.content_type() {
+        if let Some(content_type) = req.content_type() {
             match content_type.essence() {
-                "text/html"
-                | "text/javascript"
-                | "application/json"
-                | "application/manifest+json" => match resp.body_string().await {
-                    Ok(mut body) => {
-                        for (k, v) in &self.domain {
-                            body = body.replace(&v.host_with_port(), k);
-                        }
-                        resp.set_body(body);
+                "application/x-www-form-urlencoded" | "text/plain" => match req.body_string().await
+                {
+                    Ok(body) => {
+                        let body = self.restore_domain(&body);
+                        req.set_body(body);
                     }
                     Err(_) => error!("can not convert body to utf-8 string"),
                 },
@@ -208,9 +63,114 @@ impl<'a> Forward<'a> {
             }
         }
 
-        Coder::En.code(&mut resp);
+        let host = req
+            .host()
+            .map(ToString::to_string)
+            .ok_or_else(|| http_error("invalid request"))?;
+        let port = req
+            .url()
+            .port_or_known_default()
+            .ok_or_else(|| http_error("invalid request"))?;
+        let stream = match &CONFIG.socks5_server {
+            Some(server) => {
+                let server = server.clone();
+                let server = self.resolv(server).await?;
+                socks5::connect_without_auth(server, (host.clone(), port).into()).await?
+            }
+            None => {
+                let addr = format!("{}:{}", host, port);
+                let addr = self.resolv(addr).await?;
+                Async::<TcpStream>::connect(addr).await?
+            }
+        };
 
+        let mut resp = match req.url().scheme() {
+            "https" => {
+                let stream = async_native_tls::connect(host, stream).await?;
+                async_h1::connect(stream, req).await?
+            }
+            "http" => async_h1::connect(stream, req).await?,
+            s => return Err(http_error(&format!("unsupported scheme: {}", s))),
+        };
+
+        self.replace_header(&mut resp);
+
+        if resp.status() == StatusCode::NotModified {
+            return Ok(resp);
+        }
+
+        Coder::De.code(&mut resp);
+        if let Some(content_type) = resp.content_type() {
+            match content_type.essence() {
+                "text/html"
+                | "text/javascript"
+                | "application/json"
+                | "application/manifest+json" => match resp.body_string().await {
+                    Ok(body) => {
+                        let body = self.replace_domain(&body);
+                        resp.set_body(body);
+                    }
+                    Err(_) => error!("can not convert body to utf-8 string"),
+                },
+                _ => (),
+            }
+        }
+        Coder::En.code(&mut resp);
         Ok(resp)
+    }
+
+    fn replace_domain(&self, s: &str) -> String {
+        let mut result = s.to_string();
+        for (k, v) in self.domain {
+            result = result.replace(v.as_str(), k);
+        }
+        result
+    }
+
+    fn restore_domain(&self, s: &str) -> String {
+        let mut result = s.to_string();
+        for (k, v) in self.domain {
+            result = result.replace(k, v);
+        }
+        result
+    }
+
+    fn replace_header(&self, req: &mut Response) {
+        const HEADERS: &[&str] = &[
+            "location",
+            "set-cookie",
+            "access-control-allow-origin",
+            "content-security-policy",
+            "x-frame-options",
+        ];
+
+        for i in HEADERS {
+            if let Some(h) = req.header(*i) {
+                let h = self.replace_domain(h.as_str());
+                req.insert_header(*i, h);
+            }
+        }
+    }
+
+    fn restore_header(&self, req: &mut Request) {
+        const HEADERS: &[&str] = &["origin", "referer"];
+
+        for i in HEADERS {
+            if let Some(h) = req.header(*i) {
+                let h = self.restore_domain(h.as_str());
+                req.insert_header(*i, h);
+            }
+        }
+    }
+
+    async fn resolv<T: 'static + ToSocketAddrs + Send>(
+        &self,
+        s: T,
+    ) -> http_types::Result<SocketAddr> {
+        smol::unblock!(s
+            .to_socket_addrs()?
+            .next()
+            .ok_or_else(|| http_error("invalid host")))
     }
 }
 
@@ -230,6 +190,10 @@ impl Coder {
     }
 
     fn code(&self, resp: &mut Response) {
+        use async_compression::futures::bufread::{
+            BrotliDecoder, BrotliEncoder, DeflateDecoder, DeflateEncoder, GzipDecoder, GzipEncoder,
+        };
+
         if let Some(encoding) = resp.header("content-encoding") {
             let encoding = encoding.as_str();
             match encoding {
@@ -260,8 +224,8 @@ impl Coder {
     }
 }
 
-fn http_error(error: String) -> HttpError {
-    HttpError::from_str(StatusCode::InternalServerError, error)
+fn http_error(error: &str) -> HttpError {
+    HttpError::from_str(StatusCode::InternalServerError, error.to_string())
 }
 
 async fn serve(req: Request) -> http_types::Result<Response> {
